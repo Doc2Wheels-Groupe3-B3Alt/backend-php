9fc23b84e48f42bbb220e235b9daf881361ffbea
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-01-03 09:57:11 +0100

    Fonction retirer caractères spéciaux et début gestion des erreurs et conditions de création de compte

--- app/src/Controllers/RegisterController.php
+++ app/src/Controllers/RegisterController.php
@@ -15,26 +15,44 @@ class RegisterController extends AbstractController
 
     private function register()
     {
+        // error_reporting(0);
 
         $dsn = new Dsn();
         // Ajout de "dbname={$dsn->getDbName()};" pour se connecter à la base de données
         $db = new \PDO("mysql:host={$dsn->getHost()};dbname={$dsn->getDbName()};port={$dsn->getPort()}", $dsn->getUser(), $dsn->getPassword());
 
+        function RemoveSpecialChar($str)
+        {
+            $res = preg_replace('/[0-9\@\.\;\" "]+/', '', $str);
+            return $res;
+        }
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $username = $_POST['username'];
+            $username = RemoveSpecialChar($_POST['username']);
             $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-        
+            $error = "Cet identifiant est déjà utilisé";
+
+            $test = $db->query("SELECT * FROM users WHERE username='$username'");
+            $result = $test->rowCount();
+            if ($result > 0) {
+                $error = "Cet identifiant est déjà utilisé";
+            }
+
+
             $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
             $stmt->bindParam(':username', $username);
             $stmt->bindParam(':password', $password);
-        
-            if ($stmt->execute()) {
-                echo "Inscription réussie !";
-            } else {
-                echo "Erreur lors de l'inscription.";
-            }
+
+            // if ($db->query("SELECT * FROM users WHERE username='$username'")) {
+            //     echo "Cet identifiant est déjà utilisé";
+            // }
+
+            $stmt->execute([
+                'username' => $username,
+                'password' => $password
+            ]);
         }
-        
+
         ob_start();
         include __DIR__ . '/../Views/register.php';
         $content = ob_get_clean();
--- app/src/Views/register.php
+++ app/src/Views/register.php
@@ -1,20 +1,36 @@
-<form action="" method="POST">
-    <div id="titre">
-        <h1>
-            Création de compte
-        </h1>
-    </div>
+<!DOCTYPE html>
+<html lang="fr">
 
-    <hr>
-    <div id="username">
-        Identifiant : &nbsp; <input style="width: 30%;" type="text" name="username" rows="1">
-    </div>
-    <br>
-    <div id="password">
-        Mot de passe : &nbsp; <input type="password" style="width: 30%;" name="password" rows="1">
-    </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <div id="titre">
+            <h1>
+                Création de compte
+            </h1>
+            <div id="error">
+                
+            </div>
+        </div>
+
+        <hr>
+        <div id="username">
+            Identifiant : &nbsp; <input style="width: 30%;" minlength="1" type="text" name="username" rows="1">
+        </div>
+        <br>
+        <div id="password">
+            Mot de passe : &nbsp; <input type="password" minlength="1" style="width: 30%;" name="password" rows="1">
+        </div>
 
 
-    <hr>
-    <input type="submit" value="submit">
-</form>
+        <hr>
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
