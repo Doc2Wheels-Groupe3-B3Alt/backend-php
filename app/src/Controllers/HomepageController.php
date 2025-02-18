19d3f1dd2d39bb9892ac602ad1ca4dc369412d9b
Ilian Igoudjil <ilianigoudjil@gmail.com>
2025-02-19 00:30:10 +0100

    Modification de la page d'accueil

--- app/src/Controllers/HomepageController.php
+++ app/src/Controllers/HomepageController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Commands\ConnectDatabase;
 
 class HomepageController extends AbstractController
 {
@@ -15,6 +16,17 @@ class HomepageController extends AbstractController
 
     private function homepage()
     {
-        return $this->render('homepage');
+        $db = (new ConnectDatabase())->execute();
+
+        $services = $db->query("
+            SELECT 
+                id,
+                nom,
+                description
+            FROM Services
+            ORDER BY id DESC
+        ")->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $this->render('homepage', get_defined_vars());
     }
 }
--- app/src/Views/homepage.php
+++ app/src/Views/homepage.php
@@ -1,6 +1,24 @@
-<h1>Homepage</h1>
-<?php if (isset($_SESSION['user'])): ?>
-    <p>Bienvenue <?= htmlspecialchars($_SESSION['user']['username']) ?></p>
-<?php endif; ?>
-<a class="button" href="/login">Login</a>
-<a class="button" href="/register">Register</a>
+<section class="homepage-section flex-row align-items-center justify-content-center">
+    <div>
+        <img class="media" src="/src/Images/media.png" alt="Réparation">
+    </div>
+    <div class="flex-column align-items-center justify-content-space-between gap-8">
+        <img class="placeholder" src="/src/Images/Placeholder2.png" alt="Réparation">
+        <a href="/" class="button button-lg bg-light-grey c-black homepage-button">Faire un devis</a>
+    </div>
+</section>
+<section class="flex align-items-center justify-content-center homepage-section position-relative bg-light-grey">
+    <p class="description-text">Description</p>
+    <img class="description-placeholder" src="/src/Images/Placeholder2.png" alt="Description">
+</section>
+<section>
+    <h1>Nos services</h1>
+    <div class="homepage-service bg-light-grey">
+    <?php foreach ($services as $service): ?>
+        <div class="homepage-service bg-light-grey">
+            <h2><?= htmlspecialchars($service['nom']) ?></h2>
+            <p><?= htmlspecialchars($service['description']) ?></p>
+        </div>
+    <?php endforeach; ?>
+    </div>
+</section>
